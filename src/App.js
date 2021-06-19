a84ec0e18ffe88551d4cf4fe391ef705e05075d6
Your Name <you@example.com>
2021-06-19 12:04:17 +0300

    start

---
+++ src/App.js
@@ -0,0 +1,29 @@
+function App() {
+  return (
+    <div className="wrapper">
+      <header className='header'>
+        <div className='header_left'>
+        <svg/>
+        <div className='header_Info'>
+          <h3>React Sneackers</h3>
+          <p>Магазин лучших кросовок</p>
+        </div>
+        </div>
+        <ul className='header_right'>
+          <li>
+            <svg/>
+            <span>1203 руб.</span>
+          </li>
+          <li>
+            <svg/>
+          </li>
+        </ul>
+      </header>
+        <div className='content'>
+          <h1>Все кросовки</h1>
+        </div>
+    </div>
+  );
+}
+
+export default App;
