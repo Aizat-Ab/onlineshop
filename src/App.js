87a4ce2f502df73be11b6a0d5c594370e7aa5548
Your Name <you@example.com>
2021-06-24 17:10:03 +0300

    add components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,52 @@
+import React from 'react';
+import Card from './components/Card';
+import Header from './components/Header';
+import Drawer from './components/Drawer';
+
 function App() {
+  const [items, setItems] = React.useState([]);
+  const [cartItems, setCartItems] = React.useState([]);
+  const [cartOpened, setCartOpened] = React.useState(false);
+
+  React.useEffect(() => {
+    fetch('https://60d381a361160900173c93d3.mockapi.io/items')
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        setItems(json);
+      });
+  }, []);
+
+  const onAddToCart = (obj) => {
+    setCartItems((prev) => [...prev, obj]);
+  };
+
   return (
-    <div className="wrapper">
-      <header className='header'>
-        <div className='header_left'>
-        <svg/>
-        <div className='header_Info'>
-          <h3>React Sneackers</h3>
-          <p>Магазин лучших кросовок</p>
+    <div className="wrapper clear">
+      {cartOpened && <Drawer items={cartItems} onClose={() => setCartOpened(false)} />}
+      <Header onClickCart={() => setCartOpened(true)} />
+      <div className="content p-40">
+        <div className="d-flex align-center justify-between mb-40">
+          <h1>Все кроссовки</h1>
+          <div className="search-block d-flex">
+            <img src="/img/search.svg" alt="Search" />
+            <input placeholder="Поиск..." />
+          </div>
         </div>
+
+        <div className="d-flex flex-wrap">
+          {items.map((item) => (
+            <Card
+              title={item.title}
+              price={item.price}
+              imageUrl={item.imageUrl}
+              onFavorite={() => console.log('Добавили в закладки')}
+              onPlus={(obj) => onAddToCart(obj)}
+            />
+          ))}
         </div>
-        <ul className='header_right'>
-          <li>
-            <svg/>
-            <span>1203 руб.</span>
-          </li>
-          <li>
-            <svg/>
-          </li>
-        </ul>
-      </header>
-        <div className='content'>
-          <h1>Все кросовки</h1>
-        </div>
+      </div>
     </div>
   );
 }
